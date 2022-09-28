02bfe865c1dcd2efc8ee38f9d0589495fe74d91d
zamruthbegum <zamruth.m.1993@gmail.com>
2022-09-28 15:04:39 +0530

    description reuired

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -32,7 +32,7 @@ export class LoginPageComponent implements OnInit {
     this.loginForm = this.fb.group({
       firstName: ['', [Validators.required]],
       lastName: ['', [Validators.required]],
-      description: [''],
+      description: ['',[Validators.required]],
       emailaddress: ['', [Validators.required, Validators.email, Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$')]],
       file: new FormControl(''),
     });
